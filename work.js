55614782d76e6dffb460473de053f4fecb1706d3
Tushar kalaskar <119507161+tusharkalaskar2003@users.noreply.github.com>
2024-04-10 06:35:33 +0530

    work.js

--- work.js
+++ work.js
@@ -24,31 +24,31 @@ function makeSound(currentEle){
     var buttomInnerChar = currentEle;
     switch(buttomInnerChar){
         case "w":
-            var audio = new Audio("sounds/crash.mp3");
+            var audio = new Audio("crash.mp3");
             audio.play();
 
         case "a":
-            var audio = new Audio("sounds/kick-bass.mp3");
+            var audio = new Audio("kick-bass.mp3");
             audio.play(); 
         
         case "s":
-            var audio = new Audio("sounds/snare.mp3");
+            var audio = new Audio("snare.mp3");
             audio.play();
 
         case "d":
-            var audio = new Audio("sounds/tom-1.mp3");
+            var audio = new Audio("tom-1.mp3");
             audio.play(); 
         
         case "j":
-            var audio = new Audio("sounds/tom-2.mp3");
+            var audio = new Audio("tom-2.mp3");
             audio.play();
 
         case "k":
-            var audio = new Audio("sounds/tom-3.mp3");
+            var audio = new Audio("tom-3.mp3");
             audio.play(); 
         
         case "l":
-            var audio = new Audio("sounds/tom-4.mp3");
+            var audio = new Audio("tom-4.mp3");
             audio.play(); 
 
     }
